071e932bc59c125ca04402f268745ae6de1eb365
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-22 14:52:00 +0100

    changed verify to verification

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "SWPhqTaqZQ"
+    let VERIFICATION_TOKEN = "SWPhqTaqZQ"
       
     // Parse the query params
     let mode = req.query['hub.mode'];
@@ -50,7 +50,7 @@ app.get('/webhook', (req, res) => {
     if (mode && token) {
     
       // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      if (mode === 'subscribe' && token === VERIFICATION_TOKEN) {
         
         // Responds with the challenge token from the request
         console.log('WEBHOOK_VERIFIED');
