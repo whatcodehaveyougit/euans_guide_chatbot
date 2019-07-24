0f5a7545d07070a922fbabe47bb45d7cdeb5bbf4
Joseph Young <jsyoung0891@gmail.com>
2019-07-24 12:39:20 +0100

    commented out the command line prompts

--- index.js
+++ index.js
@@ -183,11 +183,11 @@ function callSendAPI(sender_psid, response, response2) {
 // }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAEZBxZBsDMYYBAJPLwYYBncuKhIZCnAVq9GrZAhkD9EwKbISZBS30D2xmmqMqKzMnFx6UE80KFFmnZAkWuy832RoWAOLCHJnivAjcggKZAO3JYmjg9Va4nng6mi0Coz8ZCyW0W8qWN4DrCFtgrjB1PxjdZC0nURiBnZBFcOcfwDOeJBvZAsqzEMFbFBWiE7MAuVP0ZD"
 
 
-curl -X POST -H "Content-Type: application/json" -d '{
-  "greeting": [
-    {
-      "locale":"default",
-      "text":"Hello {{user_first_name}}! Click on Get Started to leave your review"
-    }
-  ]
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAEZBxZBsDMYYBAJPLwYYBncuKhIZCnAVq9GrZAhkD9EwKbISZBS30D2xmmqMqKzMnFx6UE80KFFmnZAkWuy832RoWAOLCHJnivAjcggKZAO3JYmjg9Va4nng6mi0Coz8ZCyW0W8qWN4DrCFtgrjB1PxjdZC0nURiBnZBFcOcfwDOeJBvZAsqzEMFbFBWiE7MAuVP0ZD"
+// curl -X POST -H "Content-Type: application/json" -d '{
+//   "greeting": [
+//     {
+//       "locale":"default",
+//       "text":"Hello {{user_first_name}}! Click on Get Started to leave your review"
+//     }
+//   ]
+// }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAEZBxZBsDMYYBAJPLwYYBncuKhIZCnAVq9GrZAhkD9EwKbISZBS30D2xmmqMqKzMnFx6UE80KFFmnZAkWuy832RoWAOLCHJnivAjcggKZAO3JYmjg9Va4nng6mi0Coz8ZCyW0W8qWN4DrCFtgrjB1PxjdZC0nURiBnZBFcOcfwDOeJBvZAsqzEMFbFBWiE7MAuVP0ZD"
