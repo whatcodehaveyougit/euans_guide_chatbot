ecb3ee4629cb80090c527da464f4d2beb5f1c406
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-25 22:35:49 +0100

    sort buttons for Q

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
-const 
+const
   request = require('request'),
   express = require('express'),
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
 
-  let currentQuestion;
-  let handleResponse;
+let currentQuestion;
+let handleResponse;
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {  
+app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
   let body = req.body;
@@ -21,7 +21,7 @@ app.post('/webhook', (req, res) => {
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    body.entry.forEach(function(entry) {
+    body.entry.forEach(function (entry) {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
@@ -34,12 +34,12 @@ app.post('/webhook', (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
+        handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
-        
+
         handlePostback(sender_psid, webhook_event.postback);
       }
-      
+
     });
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
@@ -53,29 +53,29 @@ app.post('/webhook', (req, res) => {
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  
+
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-  
-  
+
+
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
+
   // Check if a token and mode were sent
   if (mode && token) {
-  
+
     // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+
       // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
@@ -85,30 +85,35 @@ app.get('/webhook', (req, res) => {
 // }
 
 function handleMessage(sender_psid, received_message) {
-    
+
   // Checks if the message contains text
-  if ((received_message.text !== currentQuestion) && (currentQuestion === "Can you confirm the name of the place you visited?")) {  
+  if ((received_message.text !== currentQuestion) && (currentQuestion === "Can you confirm the name of the place you visited?")) {
     handleResponse = {
       "text": `Ok, great! Can you confirm which town or city ` + received_message.text + ` is in?`
       // "text": `Ok, great! Can you confirm which town or city that is in?`
     }
     currentQuestion = handleResponse["text"]
 
-  } else if ((received_message.text !== currentQuestion) && (currentQuestion === handleResponse["text"])){
+  } else if ((received_message.text !== currentQuestion) && (currentQuestion === handleResponse["text"])) {
     handleResponse = {
       "text": `Do you have any photos or images you'd like to upload?`,
-      "buttons": [
-        {
-          "type": "postback",
-          "title": "Yes!",
-          "payload": "yes",
-        },
-        {
-          "type": "postback",
-          "title": "No!",
-          "payload": "no",
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "Yes!",
+              "payload": "yes",
+            },
+            {
+              "type": "postback",
+              "title": "No!",
+              "payload": "no",
+            }
+          ]
         }
-      ]
+      }
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
@@ -138,23 +143,23 @@ function handleMessage(sender_psid, received_message) {
         }
       }
     }
-  } 
-  
+  }
+
   // Send the response message
   callSendAPI(sender_psid, handleResponse);
-  
+
 }
 
 function handlePostback(sender_psid, received_postback) {
   console.log('ok')
-   let response;
+  let response;
   // Get the payload for the postback
   let payload = received_postback.payload;
 
   // Set the response based on the postback payload
- 
-  if(payload === 'GET_STARTED'){
-    response = {"text": "Can you confirm the name of the place you visited?"}
+
+  if (payload === 'GET_STARTED') {
+    response = { "text": "Can you confirm the name of the place you visited?" }
     currentQuestion = response["text"]
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
@@ -185,7 +190,7 @@ function callSendAPI(sender_psid, response) {
     } else {
       console.error("Unable to send message:" + err);
     }
-  }); 
+  });
 }
 
 // curl -X POST -H "Content-Type: application/json" -d '{
