2eed84c20e76d3e625c6a2f37150a7d24c2a3a05
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-25 14:49:18 +0100

    respoinse true and current question statemnt added

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function handleMessage(sender_psid, received_message) {
 
   
   // Checks if the message contains text
-  if (received_message.text == "hello") {    
+  if (received_message.text == true && currentQuestion == "Can you confirm the name of the place you visited?") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
@@ -152,6 +152,7 @@ function handlePostback(sender_psid, received_postback) {
  
   if(payload === 'GET_STARTED'){
     response = {"text": "Can you confirm the name of the place you visited?"}
+    setCurrentQuestion("Can you confirm the name of the place you visited?")
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
   }
