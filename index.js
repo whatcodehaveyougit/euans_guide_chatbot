e094b47df4685b0ee6955a881d8937afe2401fff
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-26 00:47:51 +0100

    changed wording of title Q

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function handleMessage(sender_psid, received_message) {
 
   } else if ((received_message.text === "No!") && (received_message.text !== currentQuestion) && (currentQuestion === `Do you have any photos or images you'd like to upload?`)) {
     handleResponse = {
-      "text": "No problem! Now, what would you like to title your review?"
+      "text": "Great! Now, what would you like to title your review?"
     }
     currentQuestion = handleResponse["text"]
 
@@ -124,6 +124,34 @@ function handleMessage(sender_psid, received_message) {
     }
     currentQuestion = handleResponse["text"]
 
+  // } else if (received_message.attachments) {
+  //   // Get the URL of the message attachment
+  //   let attachment_url = received_message.attachments[0].payload.url;
+  //   response = {
+  //     "attachment": {
+  //       "type": "template",
+  //       "payload": {
+  //         "template_type": "generic",
+  //         "elements": [{
+  //           "title": "Is this the right picture?",
+  //           "subtitle": "Tap a button to answer.",
+  //           "image_url": attachment_url,
+  //           "buttons": [
+  //             {
+  //               "type": "postback",
+  //               "title": "Yes!",
+  //               "payload": "yes",
+  //             },
+  //             {
+  //               "type": "postback",
+  //               "title": "No!",
+  //               "payload": "no",
+  //             }
+  //           ],
+  //         }]
+  //       }
+  //     }
+  //   }
   } else if ((received_message.text !== currentQuestion) && (currentQuestion === "Great! Now, what would you like to title your review?")) {
     handleResponse = {
       "text": "Fucking amazing title!"
