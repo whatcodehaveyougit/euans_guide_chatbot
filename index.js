deb3920db25c2ab60d381dec29695b518f284b31
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-25 16:01:07 +0100

    hello

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function handleMessage(sender_psid, received_message) {
     
   // Checks if the message contains text
   if ((received_message.text) && (currentQuestion === "Can you confirm the name of the place you visited?")) {  
-    console.log(received_message.text)  
+    console.log("HATEB THIS", received_message.text)  
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
