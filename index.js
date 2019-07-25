c76f9e459870e3652cb4d08a6f6203c91acf3558
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-25 14:56:36 +0100

    changed message

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `Welcome! Thanks for sharing your experiences of disabled access, this shouldn't take too long!`
+      "text": `Thats an amazing place.. woooohooo yeah boii`
     }
 
     // response2 = {
