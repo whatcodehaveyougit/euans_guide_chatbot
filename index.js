c89c9f64b062ae586bc5ccf4d8a7b95217d36a4a
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-25 15:59:40 +0100

    added console.log

--- index.js
+++ index.js
@@ -85,12 +85,10 @@ function setCurrentQuestion(text){
 
 function handleMessage(sender_psid, received_message) {
   let response;
-
-  // callSendAPI(sender_psid, response);    
-
-  
+    
   // Checks if the message contains text
-  if ((received_message.text) && (currentQuestion === "Can you confirm the name of the place you visited?")) {    
+  if ((received_message.text) && (currentQuestion === "Can you confirm the name of the place you visited?")) {  
+    console.log(received_message.text)  
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
