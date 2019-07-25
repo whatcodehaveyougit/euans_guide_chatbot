0eb21749cb44af25db1ecff84570e71fbd6a9af1
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-25 14:45:41 +0100

    changed message again

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function handleMessage(sender_psid, received_message) {
 
   
   // Checks if the message contains text
-  if (received_message.text == "Can you confirm the name of the place you visited?") {    
+  if (received_message.text == "hello") {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
