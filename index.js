dcee120bdf78ba9fab3807099c1dec0efa6f4d08
Alison Williams <alison.lauriewill@gmail.com>
2019-07-26 14:30:59 +0100

    Taken out extra and operator

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function handleMessage(sender_psid, received_message) {
     }
     currentQuestion = handleResponse["text"]
 
-  } else if ((received_message.attachment) && (currentQuestion === "Great, send it!") && (received_message.text !== currentQuestion) && (received_message.text === "Yes!")) {
+  } else if ((received_message.attachment) && (currentQuestion === "Great, send it!") && (received_message.text !== currentQuestion)) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
     handleResponse = {
