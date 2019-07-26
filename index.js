9751d50ddf85794ff4d1d5e8fed101d2694198f6
Alison Williams <alison.lauriewill@gmail.com>
2019-07-26 14:28:05 +0100

    Changed 'response' to 'handleResponse'

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function handleMessage(sender_psid, received_message) {
   } else if ((received_message.attachment) && (currentQuestion === "Great, send it!") && (received_message.text !== currentQuestion) && (received_message.text === "Yes!")) {
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
-    response = {
+    handleResponse = {
       "attachment": {
         "type": "template",
         "payload": {
