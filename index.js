ec0d170b509aaeb5791583418ab85e87ff74ac32
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-29 12:56:17 +0100

    move export of place

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const request = require("request"),
 let currentQuestion;
 let handleResponse;
 let place = ["code"]
+module.exports = place
 let overallRating;
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
@@ -619,8 +620,6 @@ function callSendAPI(sender_psid, response) {
     }
   );
 }
-
-module.exports = place
 // curl -X POST -H "Content-Type: application/json" -d '{
 //   "get_started": {"payload": "GET_STARTED"}
 // }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAEZBxZBsDMYYBAJPLwYYBncuKhIZCnAVq9GrZAhkD9EwKbISZBS30D2xmmqMqKzMnFx6UE80KFFmnZAkWuy832RoWAOLCHJnivAjcggKZAO3JYmjg9Va4nng6mi0Coz8ZCyW0W8qWN4DrCFtgrjB1PxjdZC0nURiBnZBFcOcfwDOeJBvZAsqzEMFbFBWiE7MAuVP0ZD"
