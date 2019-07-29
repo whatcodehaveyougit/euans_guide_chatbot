073a0766b48a3ebbc95fca996fd6d52bd06a3987
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-29 11:54:20 +0100

    hello place[0]?? haha lol

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 var place = require('./index')
 
 let questionData = [
-    `Ok, great! Can you confirm which town or city ` + place + ` is in?`
+    `Ok, great! Can you confirm which town or city ` + place[0] + ` is in?`
 ]
 
 module.exports = questionData;
