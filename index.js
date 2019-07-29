5cd210e460a4006d377aef1ae1a48c3d00d5dff6
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-29 13:09:09 +0100

    put the dot into the log

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var questionData = require('./questions');
-consolelog("QUESTIONS", questionData)
+console.log("QUESTIONS", questionData)
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
