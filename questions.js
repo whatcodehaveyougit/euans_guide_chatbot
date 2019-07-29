0f7320d3c2ba3e9278120a1d866255c2a9299bb2
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-29 11:59:03 +0100

    logging place

--- questions.js
+++ questions.js
@@ -2,7 +2,7 @@ var place = require('./index')
 
 let questionData = [
     console.log("PLAAAAAAAAAAAAAACE", place)
-    `Ok, great! Can you confirm which town or city ` + place + ` is in?`
+    `Ok, great! Can you confirm which town or city hello is in?`
 ]
 
 module.exports = questionData;
