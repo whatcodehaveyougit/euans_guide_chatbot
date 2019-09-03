355052ca841b3273b64401b2a5ebef58d915a107
Robert Tam <rsh.tam@gmail.com>
2019-09-03 13:57:24 +0100

    Change email body text

--- index.js
+++ index.js
@@ -557,7 +557,7 @@ function callSendAPI(sender_psid, response) {
     (err, res, body) => {
       if (!err) {
         console.log("message sent!", body);
-        sendEmail(response);
+        sendEmail('Test');
       } else {
         console.error("Unable to send message:" + err);
       }
