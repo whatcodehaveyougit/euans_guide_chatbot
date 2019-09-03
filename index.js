3e9aaf4451ff4dc11c4b2c089aaf924146f911db
Kyle Walker <Randomer44@hotmail.com>
2019-09-03 10:02:37 +0100

    Edited URI

--- index.js
+++ index.js
@@ -557,8 +557,8 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request(
     {
-      uri: "https://graph.facebook.com/v2.6/me/messages",
-      qs: { access_token: PAGE_ACCESS_TOKEN },
+      uri: `https://graph.facebook.com/v4.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+      // qs: { access_token: PAGE_ACCESS_TOKEN },
       method: "POST",
       json: request_body
     },
