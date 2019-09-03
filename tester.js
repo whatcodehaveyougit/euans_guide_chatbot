a14aa10b278e3bd7cf3644a2f0a8d0c4d1117ea9
Kyle Walker <Randomer44@hotmail.com>
2019-09-03 11:38:21 +0100

    Added tester.js to git ignore and generated a new token

--- tester.js
+++ tester.js
@@ -1,12 +1,12 @@
 const request = require("request");
 
-let PAGE_ACCESS_TOKEN = "EAAEZBxZBsDMYYBAJPLwYYBncuKhIZCnAVq9GrZAhkD9EwKbISZBS30D2xmmqMqKzMnFx6UE80KFFmnZAkWuy832RoWAOLCHJnivAjcggKZAO3JYmjg9Va4nng6mi0Coz8ZCyW0W8qWN4DrCFtgrjB1PxjdZC0nURiBnZBFcOcfwDOeJBvZAsqzEMFbFBWiE7MAuVP0ZD";
+let PAGE_ACCESS_TOKEN = "EAAEZBxZBsDMYYBACP9Ad3JunylpiKVpm74ioDGHMTipjOZCtwgZBGYOiFz5bk4BTMT6eIToMZAzDWvFyf5OFVZAiD0egV2OmgSf9ZBaWZAPSnoE8nP78AA3OIz0fg45ei6HuXKQqZAnq7naf6BNdxbIBTilXpBCKqU2M5iFQ8UwFfV7urFQnOX8bnpkWhEUj6EK0ZD";
 
 let request_body = {
     recipient: {
         id: 2380436998703241
     },
-    message: { text: "Hello." }
+    message: { text: "Hello Apples." }
 };
 
 request(
