35073f83a785bc876143b5318de0dc8577e3e976
Kyle Walker <Randomer44@hotmail.com>
2019-09-05 00:18:35 +0100

    Start of the rewrite

---
+++ tester.js
@@ -0,0 +1,408 @@
+function questions(questionKey, place, overallRating) {
+    const questionsArray = {
+        "hello": {question: `Hello! Thanks for clicking get started. Would you like to leave a review or chat to us?`,
+            response: {quick_replies: [
+                    {
+                        content_type: "text",
+                        title: "Review!",
+                        payload: "review"
+                    },
+                    {
+                        content_type: "text",
+                        title: "Chat!",
+                        payload: "chat"
+                    }
+                ]}},
+        "visited": {question: "Can you confirm the name of the place you visited?"},
+        "city": {question: "Ok, great! Can you confirm which town or city " + place + "is in?"},
+        "image": {question: "Do you have any photos or images you'd like to upload?", response: {quick_replies: [
+                    {
+                        content_type: "text",
+                        title: "Yes!",
+                        payload: "yes"
+                    },
+                    {
+                        content_type: "text",
+                        title: "No!",
+                        payload: "no"
+                    }
+                ]}},
+        "image2": {question: "Do you have any more photos or images you'd like to upload?", response: {quick_replies: [
+                    {
+                        content_type: "text",
+                        title: "Yes!",
+                        payload: "yes"
+                    },
+                    {
+                        content_type: "text",
+                        title: "No!",
+                        payload: "no"
+                    }
+                ]}},
+        "upload-image": {question: "Great, to select an image to attach, click on the picture icon in the bottom left corner of the messenger and send it."},
+        "title": {question: "Great! Now, what would you like to title your review?"},
+        "disabled-rating": {question: "Great Title! Now for a rating, how would you rate the disabled access overall?"},
+        "disabled-summary": {question: "You've given a rating of ` + overallRating + `. Could you summarize your experience at" + place + "?"},
+        "transport": {question: "We'll start with Getting There. Would you like to add any information on parking or transport?"},
+        "transport-rating": {question:"Ok, great! Let's start with a rating, again out of 5."},
+        "transport-summary": {question: "Awesome! Could you give us some more information?"},
+        "access": {question: "Thank You! Now onto getting in and around` + place + `. Is there anything specific about Disabled Access you would like to add?"},
+        "access-rating": {question: "Ok, great! Let's start with a rating, again out of 5 for getting in and around."},
+        "view": {question: "Great! Could you give us some more information on what you noticed about" + place + "?"},
+        "toilet": {question: "Now, onto toilets. Our users consistently tell us how important both accessible toilets and information about toilets is. Are you able to tell us anything about the toilets at" + place + "?"},
+        "toilet-rating": {question: "Ok, great! Let's start with a rating, again out of 5 for toilet accessibility."},
+        "toilet-summary": {question: "Would you be able to provide some more details about the toilets?"},
+        "staff": {question: "Now we come to staff. " +
+                "Would you like to add any further information about the people you came across at" + place + "?"},
+            "staff-rating":  {question: "Ok, great! Let's start with a rating, again out of 5 for staff."},
+        "staff-summary": {question: "Would you be able to provide some more details about the staff?"}
+    };
+    return questionsArray[questionKey];
+}
+
+function handleMessage (sender_psid, received_message) {
+
+    if (
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(0, place, overallRating) &&
+        received_message.text === `Review!`
+    ) {
+        handleResponse = {
+            text: questions(1, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(1, place, overallRating)
+    ) {
+        place = received_message.text;
+        handleResponse = {
+            text: questions(2, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(2, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(3, place, overallRating),
+            quick_replies: [
+                {
+                    content_type: "text",
+                    title: "Yes!",
+                    payload: "yes"
+                },
+                {
+                    content_type: "text",
+                    title: "No!",
+                    payload: "no"
+                }
+            ]
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text === "No!" &&
+        received_message.text !== currentQuestion &&
+        (currentQuestion === questions(3, place, overallRating) ||
+            currentQuestion === questions(4, place, overallRating))
+    ) {
+        handleResponse = {
+            text: questions(6, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text === "Yes!" &&
+        received_message.text !== currentQuestion &&
+        (currentQuestion === questions(3, place, overallRating) ||
+            currentQuestion === questions(4, place, overallRating))
+    ) {
+        handleResponse = {
+            text: questions(5, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.attachments &&
+        currentQuestion === questions(5, place, overallRating) &&
+        received_message.text !== currentQuestion
+    ) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        handleResponse = {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: [
+                        {
+                            title: "Is this the right picture?",
+                            subtitle: "Tap a button to answer.",
+                            image_url: attachment_url,
+                            buttons: [
+                                {
+                                    type: "postback",
+                                    title: "Yes!",
+                                    payload: "yes"
+                                },
+                                {
+                                    type: "postback",
+                                    title: "No!",
+                                    payload: "no"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        };
+    } else if (
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(6, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(7, place, overallRating),
+            quick_replies: ratings
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text === "1" || "2" || "3" || "4" || "5") &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(7, place, overallRating)
+    ) {
+        overallRating = received_message.text;
+        handleResponse = {
+            text: questions(8, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(8, place, overallRating)
+    ) {
+        handleResponse = {
+            text: `Thank you very much your review is nearly complete!`,
+            quick_replies: [
+                {
+                    content_type: "text",
+                    title: "Continue",
+                    payload: "continue_option_question"
+                },
+                {
+                    content_type: "text",
+                    title: "Finish",
+                    payload: "finish_option_question"
+                }
+            ]
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text === "Finish" &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === `Thank you very much your review is nearly complete!`
+    ) {
+        handleResponse = {
+            text: `Thank you for your review - it's great. We'll send you a message when it has gone live! :)`
+        };
+        currentQuestion = handleResponse["text"];
+        finish(sender_psid);
+        sendEmail(userAnswers);
+    } else if (
+        received_message.text === "Continue" &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === `Thank you very much your review is nearly complete!`
+    ) {
+        handleResponse = {
+            text: questions(9, place, overallRating),
+            quick_replies: [
+                {
+                    content_type: "text",
+                    title: "Yes",
+                    payload: "yes_get_there"
+                },
+                {
+                    content_type: "text",
+                    title: "Skip",
+                    payload: "skip_get_there"
+                }
+            ]
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text === "Yes" &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(9, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(10, place, overallRating),
+            quick_replies: ratings
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text === "1" || "2" || "3" || "4" || "5") &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(10, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(11, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text || received_message.text === "Skip") &&
+        received_message.text !== currentQuestion &&
+        (currentQuestion === questions(11, place, overallRating) ||
+            currentQuestion === questions(9, place, overallRating))
+    ) {
+        handleResponse = {
+            text: questions(12, place, overallRating),
+            quick_replies: [
+                {
+                    content_type: "text",
+                    title: "Yes",
+                    payload: "yes_disabled_access"
+                },
+                {
+                    content_type: "text",
+                    title: "Skip",
+                    payload: "skip_disabled_access"
+                }
+            ]
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text === "Yes" &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(12, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(13, place, overallRating),
+            quick_replies: ratings
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text === "1" || "2" || "3" || "4" || "5") &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(13, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(14, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text || received_message.text === "Skip") &&
+        received_message.text !== currentQuestion &&
+        (currentQuestion === questions(14, place, overallRating) ||
+            currentQuestion === questions(12, place, overallRating))
+    ) {
+        {
+            handleResponse = {
+                text: questions(15, place, overallRating),
+                quick_replies: [
+                    {
+                        content_type: "text",
+                        title: "Yes",
+                        payload: "yes_disabled_access"
+                    },
+                    {
+                        content_type: "text",
+                        title: "Skip",
+                        payload: "skip_disabled_access"
+                    }
+                ]
+            };
+            currentQuestion = handleResponse["text"];
+        }
+    } else if (
+        received_message.text === "Yes" &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(15, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(16, place, overallRating),
+            quick_replies: ratings
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text === "1" || "2" || "3" || "4" || "5") &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(16, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(17, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text || received_message.text === "Skip") &&
+        received_message.text !== currentQuestion &&
+        (currentQuestion === questions(15, place, overallRating) ||
+            currentQuestion === questions(17, place, overallRating))
+    ) {
+        handleResponse = {
+            text: questions(18, place, overallRating),
+            quick_replies: [
+                {
+                    content_type: "text",
+                    title: "Yes",
+                    payload: "yes_disabled_access"
+                },
+                {
+                    content_type: "text",
+                    title: "Skip",
+                    payload: "skip_disabled_access"
+                }
+            ]
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text === "Yes" &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(18, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(19, place, overallRating),
+            quick_replies: ratings
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text === "1" || "2" || "3" || "4" || "5") &&
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(19, place, overallRating)
+    ) {
+        handleResponse = {
+            text: questions(20, place, overallRating)
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        (received_message.text || received_message.text === "Skip") &&
+        received_message.text !== currentQuestion &&
+        (currentQuestion === questions(18, place, overallRating) ||
+            currentQuestion === questions(20, place, overallRating))
+    ) {
+        handleResponse = {
+            text: `Thank you for your review - it's great. We'll send you a message when it has gone live! :)`
+        };
+        currentQuestion = handleResponse["text"];
+        finish(sender_psid)
+        sendEmail(userAnswers);
+    } else if (
+        received_message.text !== currentQuestion &&
+        currentQuestion === questions(0, place, overallRating) &&
+        received_message.text === "Chat!"
+    ) {
+        handleResponse = {
+            text: `No problem! Leave us a message and we will get back to you as soon as possible!`
+        };
+        currentQuestion = handleResponse["text"];
+    } else if (
+        received_message.text !== currentQuestion &&
+        (currentQuestion ===
+            `Thank you for your review - it's great. We'll send you a message when it has gone live! :)` ||
+            currentQuestion ===
+            `No problem! Leave us a message and we will get back to you as soon as possible!`)
+    ) {
+        handleResponse = {
+            text: `Uh oh. Something's went wrong. Try deleting the chat and starting again. Sorry!`
+        };
+        currentQuestion = handleResponse["text"];
+    }
+
+    // Send the response message
+    callSendAPI(sender_psid, handleResponse);
+}
