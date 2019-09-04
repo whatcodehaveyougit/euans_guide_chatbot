c382e6bc78364528f4aaa51fa6e4f7c651221801
Robert Tam <rsh.tam@gmail.com>
2019-09-04 09:51:52 +0100

    convert userAnswer object to string

--- index.js
+++ index.js
@@ -583,7 +583,7 @@ function sendEmail(review) {
     from: process.env.EMAIL_ACCOUNT,
     to: process.env.EMAIL_RECIPIENT,
     subject: 'Sending Review from Facebook bot',
-    text: review
+    text: String(review)
   };
 
   transporter.sendMail(mailOptions, function(error, info){
