aa6d638b0cecb11c5c255e610681217db6829ca1
Robert Tam <rsh.tam@gmail.com>
2019-09-09 15:11:14 +0100

    add branch for user submit keyword

--- index.js
+++ index.js
@@ -231,6 +231,8 @@ class chatBot {
       return null;
     } else if (received_message.text.toLowerCase() === "stop") {
       this.currentQuestion = "user-stop";
+    } else if (received_message.text.toLowerCase() === "submit") {
+      this.currentQuestion = "user-submit";
     }
 
     let attachment_url = null;
@@ -343,6 +345,9 @@ class chatBot {
         break;
       case "stop":
         this.currentQuestion = "visited";
+        break;
+      case "user-submit":
+        this.currentQuestion = "end";
         break;
 
         ///disabled summary needs overallRating = received_message.text;
