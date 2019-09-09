dc020c6be0e198274b0d9251d00cba2fc3844d5a
Robert Tam <rsh.tam@gmail.com>
2019-09-09 11:42:14 +0100

    filter end key from email

--- index.js
+++ index.js
@@ -453,7 +453,7 @@ class chatBot {
     let formattedString = string.split("\n");
     formattedString.shift();
 
-    formattedString = formattedString.filter(str => !(str.includes("image :") || str.includes("image2 :") || str.includes("continue-or-finish :") || str.includes("transport :") || str.includes("access :") || str.includes("toilet :") || str.includes("staff :")));
+    formattedString = formattedString.filter(str => !(str.includes("image :") || str.includes("image2 :") || str.includes("continue-or-finish :") || str.includes("transport :") || str.includes("access :") || str.includes("toilet :") || str.includes("staff :") || str.includes("end :")));
 
     formattedString = formattedString.join("\n");
 
