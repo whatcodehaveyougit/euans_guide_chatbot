456b34c14db95bef2ebb29bb680160bf594b7812
Robert Tam <rsh.tam@gmail.com>
2019-09-09 13:52:12 +0100

    add code to check for keyword exit to exit out of review at any point

--- index.js
+++ index.js
@@ -216,10 +216,14 @@ class chatBot {
     this.images = [];
   }
 
-  handleMessage(received_message){
-    if (received_message.is_echo===true)
+  handleMessage(received_message, sender_psid){
+    if (received_message.is_echo === true) {
       return null;
-    else if (received_message.text==="")
+    } else if (received_message.text === "exit") {
+      let index = botInstances.findIndex(() => this.userId === sender_psid);
+      this.currentQuestion = "";
+      botInstances.splice(index, 1);
+    }
 
     let attachment_url = null;
     let attachment_response = null;
@@ -518,7 +522,7 @@ app.post("/webhook", (req, res) => {
       // pass the event to the appropriate handler function
       if (webhook_event.message || webhook_event.attachments) {
         currentBot.userAnswers[currentBot.currentQuestion] = webhook_event.message.text;
-        currentBot.handleMessage(webhook_event.message);
+        currentBot.handleMessage(webhook_event.message, sender_psid);
       } else if (webhook_event.postback) {
         currentBot.userAnswers[currentBot.currentQuestion] = webhook_event.postback.text;
         currentBot.handlePostback(webhook_event.postback);
